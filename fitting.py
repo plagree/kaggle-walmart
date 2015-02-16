cc88b468649484e6a64f109bc82f62c6bd3890cd
plagree <paul.lagree@gmail.com>
2015-02-16 09:50:59 +0100

    Better regression, combination Lasso+Random Forests+Gradient Boosting, 2998 to 2876 in private leaderboard

--- fitting.py
+++ fitting.py
@@ -2,7 +2,26 @@
 #-*-coding: utf-8 -*-
 
 import pandas as pd
+import numpy as np
 from sklearn import *
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.base import RegressorMixin, BaseEstimator
+
+class EnsembleRegressor(BaseEstimator, RegressorMixin):
+    def __init__(self, regressors=None):
+        self.regressors = regressors
+
+    def fit(self, X, y):
+        for regressor in self.regressors:
+            regressor.fit(X, y)
+
+    def predict(self, X):
+        self.predictions_ = list()
+        for regressor in self.regressors:
+            self.predictions_.append(regressor.predict(X))
+        return np.mean(self.predictions_, axis=0)
 
 
 def fit_regressors():
@@ -11,8 +30,8 @@ def fit_regressors():
         for spec in ["", "spec"]:
             print Type, " ", spec
             X = pd.read_table(Type+spec+"values.csv", sep=',',
-                              warn_bad_lines=True,
-                              error_bad_lines=True)
+                    warn_bad_lines=True,
+                    error_bad_lines=True)
             X = X.drop("Date", 1)
             X = X.drop("Store", 1)
             X = X.drop("Dept", 1)
@@ -20,15 +39,19 @@ def fit_regressors():
             X = X.drop("IsHoliday", 1)
             Y = X["Weekly_Sales3"]
             X = X.drop("Weekly_Sales3", 1)
-            
+
             # X1 = preprocessing.scale(X1)
             # X2 = preprocessing.scale(X2)
-            
+
             # rbf_svc = svm.SVC(kernel='rbf',
             #                   cache_size=1000)
-            
+
             # rbf_svc.fit(X1, Y1)
-            clf = linear_model.LassoLars(alpha = 0.1)
-            clf.fit(X, Y)
-            dico[Type+spec] = clf
+            r1 = linear_model.LassoLars(alpha = 0.1)
+            r2 = RandomForestRegressor(n_estimators=200)
+            #clf = AdaBoostRegressor(base_estimator=RandomForestRegressor(n_estimators=15),n_estimators=50)
+            r3 = GradientBoostingRegressor(loss='lad', n_estimators=200, max_depth=7)
+            reg = EnsembleRegressor([r1,r2,r3,r3])
+            reg.fit(X, Y)
+            dico[Type+spec] = reg
     return dico
